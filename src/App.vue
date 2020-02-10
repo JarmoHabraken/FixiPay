ce221cc88b92ae333b899b24d32b9ef971a3571f
JarmoHabraken <jarmo.habraken@gmail.com>
2020-02-10 15:16:30 +0100

    made a component out of the program cards.

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,6 @@
 <template>
     <v-app id="inspire">
-        <v-navigation-drawer
-                v-model="drawer"
-                app
-                color="accent"
-        >
-            <v-list dense>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon class="secondary--text">mdi-home</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title class="secondary--text">Featured</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon class="secondary--text">mdi-contact-mail</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title class="secondary--text">Your Profile</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-
-        <v-app-bar
-                app
-                color="secondary"
-        >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="accent--text"/>
-            <v-toolbar-title class="accent--text display-1" >FixiPay</v-toolbar-title>
-            <div class="pl-3">
-                <v-img src="./Images/icon3.png" height="50" width="75"/>
-            </div>
-            <v-flex class="searchbar-positioning align-content-center">
-                <div class="pt-7">
-                    <v-text-field rounded solo class="" label="Search" append-icon="fas fa-search" style="width: 750px;"></v-text-field>
-                </div>
-            </v-flex>
-            <div class="px-5">
-                <v-btn fab><v-icon class="fas fa-user"></v-icon></v-btn>
-            </div>
-        </v-app-bar>
+        <nav-drawer/>
 
         <v-content>
             <header-card/>
@@ -142,6 +100,7 @@
 
     import HeaderCard from "./components/HeaderCard";
     import ProgramCards from "./components/ProgramCards";
+    import NavDrawer from "./components/NavDrawer";
 
     export default {
         name: 'App',
@@ -149,13 +108,8 @@
         components: {
             HeaderCard,
             ProgramCards,
+            NavDrawer,
         },
-        props: {
-            source: String,
-        },
-        data: () => ({
-            drawer: false,
-        }),
     }
 </script>
 
---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,63 @@
+<template>
+    <App>
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                color="accent"
+        >
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon class="secondary--text">mdi-home</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="secondary--text">Featured</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon class="secondary--text">mdi-contact-mail</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="secondary--text">Your Profile</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+                app
+                color="secondary"
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="accent--text"/>
+            <v-toolbar-title class="accent--text display-1" >FixiPay</v-toolbar-title>
+            <div class="pl-3">
+                <v-img src="../Images/icon3.png" height="50" width="75"/>
+            </div>
+            <v-flex class="searchbar-positioning align-content-center">
+                <div class="pt-7">
+                    <v-text-field rounded solo class="" label="Search" append-icon="fas fa-search" style="width: 750px;"></v-text-field>
+                </div>
+            </v-flex>
+            <div class="px-5">
+                <v-btn fab><v-icon class="fas fa-user"></v-icon></v-btn>
+            </div>
+        </v-app-bar>
+    </App>
+</template>
+
+<script>
+    export default {
+        name: "NavDrawer.vue",
+        props: {
+            source: String,
+        },
+        data: () => ({
+            drawer: false,
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
