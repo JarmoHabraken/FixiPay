227c53fd9f0cbda6b9830622c530cbc5d463cf54
JarmoHabraken <jarmo.habraken@gmail.com>
2020-02-10 15:07:05 +0100

    made a component out of the program cards.

--- src/App.vue
+++ src/App.vue
@@ -47,69 +47,6 @@
         <v-content>
             <header-card/>
             <program-cards/>
-            <v-row class="">
-                <v-col class="pa-12">
-                    <v-card max-width="100%" class="elevation-5">
-                        <v-container class="pa-5">
-                            <div class="headline secondary--text font-weight-bold">Illustrator Specialist</div>
-                            <v-row class="pa-3 justify-space-around">
-                                <v-card class="elevation-0" max-width="60%">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                                    nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                    irure
-                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt
-                                    mollit anim id est laborum.
-                                </v-card>
-                                <v-card class="elevation-0" max-width="40%">
-                                    <v-img src="./Images/Adobe_Illustrator_Icon_CS6.png" height="200"
-                                           width="200"></v-img>
-                                </v-card>
-                            </v-row>
-                            <v-divider class="py-3"></v-divider>
-                            <v-row class="d-flex justify-end pr-9">
-                                <v-btn class="ma-3 secondary--text" outlined>Contact now</v-btn>
-                                <v-btn class="ma-3 secondary--text" outlined>Contact now</v-btn>
-                            </v-row>
-                        </v-container>
-                    </v-card>
-                </v-col>
-                <v-col class="pa-12">
-                    <v-card max-width="100%" class="elevation-5">
-                        <v-container class="pa-5">
-                            <div class="headline secondary--text font-weight-bold">Illustrator Specialist</div>
-                            <v-row class="pa-3 justify-space-around">
-                                <v-card class="elevation-0" max-width="60%">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                                    nostrud
-                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                    irure
-                                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                    pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                    deserunt
-                                    mollit anim id est laborum.
-                                </v-card>
-                                <v-card class="elevation-0" max-width="40%">
-                                    <v-img src="./Images/logo_brand_brands_logos_adobe_photoshop-512.png"
-                                           height="200"
-                                           width="200"/>
-                                </v-card>
-                            </v-row>
-                            <v-divider class="py-3"></v-divider>
-                            <v-row class="d-flex justify-end pr-9">
-                                <v-btn class="ma-3 secondary--text" outlined>Contact now</v-btn>
-                                <v-btn class="ma-3 secondary--text" outlined>Contact now</v-btn>
-                            </v-row>
-                        </v-container>
-                    </v-card>
-                </v-col>
-            </v-row>
             <v-row
                     align="center"
                     justify="center"
