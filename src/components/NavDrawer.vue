5a61abc11a2918c914fea7826ba0825a1da677b2
JarmoHabraken <jarmo.habraken@gmail.com>
2020-02-12 15:16:02 +0100

    updated NavDrawer.vue.

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -28,19 +28,23 @@
         <v-app-bar
                 app
                 color="secondary"
+                height="90px"
         >
             <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="accent--text"/>
-            <v-toolbar-title class="accent--text display-1" >FixiPay</v-toolbar-title>
-            <div class="pl-3">
-                <v-img src="../Images/icon3.png" height="50" width="75"/>
+            <v-toolbar-title class="accent--text display-1">FixiPay</v-toolbar-title>
+            <div class="pl-5">
+                <v-img src="../Images/icon4 - kopie.png" height="50" width="90"/>
             </div>
-            <v-flex class="searchbar-positioning align-content-center">
+            <v-flex class="searchbar-positioning">
                 <div class="pt-7">
-                    <v-text-field rounded solo class="" label="Search" append-icon="fas fa-search" style="width: 750px;"></v-text-field>
+                    <v-text-field rounded solo class="" label="Search" append-icon="fas fa-search"
+                                  style="width: 750px;"></v-text-field>
                 </div>
             </v-flex>
             <div class="px-5">
-                <v-btn fab><v-icon class="fas fa-user"></v-icon></v-btn>
+                <v-btn fab class="accent">
+                    <v-icon class="fas fa-user"></v-icon>
+                </v-btn>
             </div>
         </v-app-bar>
     </App>
@@ -59,5 +63,8 @@
 </script>
 
 <style scoped>
-
+    .searchbar-positioning {
+        display: flex;
+        justify-content: center;
+    }
 </style>
